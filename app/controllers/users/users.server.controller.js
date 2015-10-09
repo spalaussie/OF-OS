0d41eff171fea820ae70a71b376c7a4d8791aedf
spalaussie <spalaussie@gmail.com>
2015-10-09 13:11:43 +1100

    Copied first time

---
+++ app/controllers/users/users.server.controller.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var _ = require('lodash'),
+    errorHandler = require('../errors.server.controller.js'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    User = mongoose.model('User');
+
+/**
+ * Update user details
+ */
+
+exports.userByID = function(req, res, next, id) {
+    User.findById(id).populate('user', 'email').exec(function(err, user) {
+        if (err) return next(err);
+        if (! user) return next(new Error('Failed to load User ' + id));
+        req.user = user ;
+        next();
+    });
+};
